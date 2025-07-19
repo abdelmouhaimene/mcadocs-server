ca2520b04b42df2c0a657f3b17b0477fc5191cc1
abdelmouhaimene <mimou4ever1999@gmail.com>
2025-07-19 23:02:43 +0100

    create admin service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuhtModule } from './auth/auht.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [AuhtModule, UserModule, BookmarkModule],
+  imports: [AuhtModule, UserModule, BookmarkModule, AdminModule],
   controllers: [],
   providers: [],
 })
